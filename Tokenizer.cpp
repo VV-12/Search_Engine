891959d32ba0f5e88d8a035cf3bb366cdf37ba0f
Vaibhav Vashist <vaibhavvashist2005@gmail.com>
2026-08-05 22:47:32 +0530

    Removed unused header file inclusion from 'Tokenizer.cpp' and simplified definiton of 'tokenizer' function

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -1,28 +1,21 @@
 #include "Tokenizer.h"
-#include "DocumentLoader.h"
-#include <iostream>
-
 
 void tokenizer(std::vector <Document>& loadedDocs) {
-    std::vector <std::vector <std::string>> tokenizedDocs;
 
     for (Document& doc : loadedDocs) {
         std::string currWord = "";
 
         for (char c : doc.content) {
-            if (c >= 65 && c <= 90) {
-                currWord += (c+32);
-            }
-            else if (c >= 97 && c <= 122) {
-                currWord += c;
+            if (std::isalpha(c)) {
+                currWord += std::tolower(c);
             }
             else {
-                if (currWord != "") doc.tokens.push_back(currWord);
+                if (!currWord.empty()) doc.tokens.push_back(currWord);
                 currWord = "";
             }
         }
 
-        if (currWord != "") {
+        if (!currWord.empty()) {
             doc.tokens.push_back(currWord);
             currWord = "";
         }
